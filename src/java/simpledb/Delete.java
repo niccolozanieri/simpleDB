483ecf055e4f38370c030cb2104eebc1c09afc1b
Giulia Bonino <60091519+GiuliaBonino@users.noreply.github.com>
2024-01-16 09:16:07 +0100

    implemented Delete

--- src/java/simpledb/Delete.java
+++ src/java/simpledb/Delete.java
@@ -19,25 +19,39 @@ public class Delete extends Operator {
      * @param child
      *            The child operator from which to read tuples for deletion
      */
+    private TransactionId t;
+    private OpIterator child;
+    private boolean finished;
     public Delete(TransactionId t, OpIterator child) {
         // some code goes here
+    	this.t=t;
+    	this.child=child;
+    	this.finished=false;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+    	Type[] types = new Type[] {Type.INT_TYPE };
+    	String[] fields = new String[] { null };
+    	TupleDesc t=new TupleDesc(types, fields);
+        return t;
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+    	child.open();
+    	super.open();
     }
 
     public void close() {
         // some code goes here
+    	child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+    	child.rewind();
+    	this.finished=false;
     }
 
     /**
@@ -51,18 +65,49 @@ public class Delete extends Operator {
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+    	if (this.finished)
+    		return null;
+    	Type[] types = new Type[] {Type.INT_TYPE }; 
+    	String[] fields = new String[] {null };
+    	TupleDesc td = new TupleDesc(types, fields);
+    	Tuple tup = new Tuple(td);
+    	int count=0;
+        while(child.hasNext()) {
+        	try {
+        		Database.getBufferPool().deleteTuple(this.t, child.next());;
+        	}
+        	catch (TransactionAbortedException e) {
+        		throw new TransactionAbortedException();
+        	}
+        	catch(DbException d) {
+        		throw new DbException ("insertion of tuple failed");
+        		}
+        	catch (IOException i) {
+        		//TODO
+        	}
+        	count=count+1;       	
+        }
+        IntField f= new IntField(count);
+        tup.setField(0, f);
+        this.finished=true;
+        return tup;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[] {this.child};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+    	if (children.length==1) {
+    		this.child=children[0];
+    	}
+    	else {
+    		//TODO
+    	}
     }
 
 }
