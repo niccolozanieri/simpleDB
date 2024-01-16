2dcf7cc10d7ff4a18f6208072a296b1b63e1b048
niccolozanieri <niccolo.zanieri.13@gmail.com>
2024-01-16 17:09:55 +0100

    Removed commented code from HeapFile

--- src/java/simpledb/HeapFile.java
+++ src/java/simpledb/HeapFile.java
@@ -152,54 +152,14 @@ public class HeapFile implements DbFile {
     		
     	}
     	//if there is no space, we create a new page
-    	HeapPageId pid =new HeapPageId(this.getId(), this.numPages()); 
-    	HeapPage newp= new HeapPage(pid, new byte[bf.getPageSize()]);
+    	HeapPageId pid = new HeapPageId(this.getId(), this.numPages()); 
+    	HeapPage newp = new HeapPage(pid, new byte[bf.getPageSize()]);
     	writePage(newp);
     	newp.insertTuple(t);
     	return new ArrayList<Page> (Arrays.asList(newp));   	
         
         
     }
-    
-//    public ArrayList<Page> insertTuple(TransactionId tid, Tuple t)
-//            throws DbException, IOException, TransactionAbortedException {
-//        // some code goes here
-//     
-//     BufferPool bf= Database.getBufferPool();
-//     
-//     try {
-//      int i=0;
-//      HeapPageId pid = new HeapPageId(this.getId(), i);
-//      HeapPage hp=(HeapPage)bf.getPage(tid,pid, Permissions.READ_WRITE);
-//      
-//      while(hp!=null) {
-//       
-//       hp=(HeapPage)bf.getPage(tid,pid, Permissions.READ_WRITE);
-//       
-//       if(hp.getNumEmptySlots()>0) {
-//        try {
-//            hp.insertTuple(t);
-//           }
-//           catch (DbException d) {
-//            throw new DbException(d.getMessage()); //should not happen
-//           }
-//           return new ArrayList<Page> (Arrays.asList(hp));
-//           
-//       }
-//       
-//       i++;
-//       
-//      }
-//     } catch(DbException e) {
-//      HeapPageId pid =new HeapPageId(this.getId(), this.numPages()); 
-//         HeapPage newp= new HeapPage(pid, new byte[bf.getPageSize()]);
-//         newp.insertTuple(t);
-//         return new ArrayList<Page> (Arrays.asList(newp)); 
-//      
-//     }
-//     //if there is no space, we create a new page
-//     return null;
-//    }
 
     // see DbFile.java for javadocs
     public ArrayList<Page> deleteTuple(TransactionId tid, Tuple t) throws DbException,
